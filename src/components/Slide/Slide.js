c8a1d8b687cb17d7029c61e5b922da27fafae6a7
Hughes89 <mik3h89@gmail.com>
2017-04-10 15:18:01 -0400

    Feat: update slide height to 50%

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export const Slide = ({color, image, i, children}) => {
-  // color = color || 'white';
-  console.log(i)
   const styles = {
     width: '100%',
-    height: '80%',
+    height: '50%',
     margin: '0',
     padding: '110px 0 260px 0',
     background: image ? `url(${image}) 50% 0 no-repeat fixed` : '50% 0 no-repeat fixed',
