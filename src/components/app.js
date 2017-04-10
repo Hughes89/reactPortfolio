b41b2303285169bf8d4fc2fc4c5993175d681670
Hughes89 <mik3h89@gmail.com>
2017-04-10 15:18:30 -0400

    Feat: add project component to slide array

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navigation } from './Navigation/Navigation';
 import { Slide } from './Slide/Slide';
 import { Skills } from './Skills/Skills';
+import { Projects } from './Projects/Projects';
 import './app.css';
 
 
@@ -9,7 +10,7 @@ const App = () => {
   const slides = [
     {color: 'white', image: 'https://c1.staticflickr.com/3/2899/33800958416_a16b44aac9_o.jpg', component: ''},
     {color: '#f9f9f9', image: false, component: <Skills />},
-    {color: '#c6c6c6', image: false, component: '<div>Hello World</div>'},
+    {color: '#c6c6c6', image: false, component: <Projects />},
     {color: '#f9f9f9', image: false, component: '<div>ABOUTME</div>'},
     {color: '#c6c6c6', image: false, component: '<div>CONTACTME</div>'}
     ];
