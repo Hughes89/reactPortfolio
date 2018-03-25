40f38d5c0ca5813397dfcfa7702420c58480e038
Hughes89 <mik3h89@gmail.com>
2018-03-25 15:37:32 -0400

    Removed route config use one route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const port = process.env.port || 1337;
 
 app.use(express.static('public'));
 
-require('./config/routes.js')(app, express);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
 
 app.listen(port, () => {
   console.log('Server listening on port: ' + port);
