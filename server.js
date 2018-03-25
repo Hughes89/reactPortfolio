4d341fa971caf0f4c61e0bd339814aa2d058516b
Hughes89 <mik3h89@gmail.com>
2018-03-25 15:48:14 -0400

    Capitalize port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const port = process.env.port || 1337;
+const port = process.env.PORT || 1337;
 
 app.use(express.static('public'));
 
