19f29dacceb7269519fc8d54b3075481cf532220
Hughes89 <mik3h89@gmail.com>
2018-03-26 18:52:53 -0400

    Grammar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ export const About = () => (
   <div className="about-container">
     <h1>About</h1>
     <div className="about-content">
-        I'm a full stack software engineer and a graduate of Hack Reactor, and am currently a Solutions Architect at <a href="https://www.feith.com/" target="blank">Feith Systems and Software</a>. With a passion for JavaScript I enjoy debugging problems, teaching, helping others, building applications, and expanding my knowledge of frameworks. I thrive in fast pace environments, and believe in growing as a team.
+        I'm a full stack software engineer and a graduate of Hack Reactor. I am currently a Solutions Architect at <a href="https://www.feith.com/" target="blank">Feith Systems and Software</a>. With a passion for JavaScript, I enjoy debugging problems, teaching, helping others, building applications, and expanding my knowledge of frameworks. I thrive in fast pace environments, and believe in growing as a team.
     </div>
     <div className="arrow-container">
       <a href="#slide4">
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -29,7 +29,7 @@ export const Contact = () => {
     <div className="contact-container">
       <h1>Contact</h1>
       <div className="contact-content">
-      <p>If you wanna get in touch, talk to me about a project collaboration or just say hi. Simply shoot an email to <a href="mailto:mik3h89@gmail.com">mik3h89@gmail.com</a>.</p>
+      <p>If you want to get in touch, talk to me about a project collaboration, or just say hi. Simply shoot an email to <a href="mailto:mik3h89@gmail.com">mik3h89@gmail.com</a>.</p>
       {/* <div className="contact-form">
         <div className="contact-label">
           <label htmlFor="fullname"><span>Full Name</span></label>
@@ -57,7 +57,7 @@ export const Contact = () => {
       <div className="center">
         <button className="contact-button" onClick={ () => sendMessage()}>Send Message</button>
       </div> */}
-      <p>Of course you can always find me on GitHub and LinkedIn.<br />
+      <p>You can always find me on GitHub and LinkedIn.<br />
       <span className="social-icon">
         <a href="https://github.com/Hughes89" target="blank"><i className="fa fa-github fa-2x" aria-hidden="true" title="Github"></i></a>
       </ span>
