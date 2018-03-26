d90c7ebf10fc7dce0ccf9061f64f56ec4331312c
Hughes89 <mik3h89@gmail.com>
2018-03-25 22:18:19 -0400

    Midway through form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -16,21 +16,49 @@ export const Contact = () => {
     field[fieldName] = e.target.value;
   };
 
-  const sendEmail = () => {
-    console.log(name);
-    console.log(field);
+  const isDisabled = () => {
+    return field.name.length > 0 && field.email.length > 0 && field.message.length > 0;
   };
 
   const sendMessage = () => {
-
+    console.log(field)
+    if (!isDisabled()) {
+      console.log('submit');
+    }
   };
 
   return (
     <div className="contact-container">
       <h1>Contact</h1>
       <div className="contact-content">
-      <p>If you wanna get in touch, talk to me about a project collaboration or just say hi, simply shoot an email to <a href="mailto:mik3h89@gmail.com">mik3h89@gmail.com</a>.</p>
-
+      <p>If you wanna get in touch, talk to me about a project collaboration or just say hi. Simply fill out the form below or just shoot an email to <a href="mailto:mik3h89@gmail.com">mik3h89@gmail.com</a>.</p>
+      <div className="contact-form">
+        <div className="contact-label">
+          <label htmlFor="fullname"><span>Full Name</span></label>
+        </div>
+        <div className="contact-input">
+          <input type="text" placeholder="Michael Hughes" name="fullname"/>
+        </div>
+      </div>
+      <div className="contact-form">
+        <div className="contact-label">
+          <label htmlFor="email"><span>Email Address</span></label>
+        </div>
+        <div className="contact-input">
+          <input type="text" placeholder="mik3h89@gmail.com" name="email"/>
+        </div>
+      </div>
+      <div className="contact-form">
+        <div className="contact-label">
+          <label htmlFor="message"><span>Message</span></label>
+        </div>
+        <div className="contact-input">
+          <textarea type="text" placeholder="" name="message" rows="4"/>
+        </div>
+      </div>
+      <div className="center">
+        <button className="contact-button" onClick={ () => sendMessage()}>Send Message</button>
+      </div>
       <p>Of course you can always find me on GitHub and LinkedIn.<br />
       <span className="social-icon">
         <a href="https://github.com/Hughes89" target="blank"><i className="fa fa-github fa-2x" aria-hidden="true" title="Github"></i></a>
